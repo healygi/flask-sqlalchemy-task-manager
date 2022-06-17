54b4efb77719842603dafedf6e239192d3f78a93
healygi <healygi@tcd.ie>
2022-06-17 09:56:43 +0000

    Add catagory files to app and ability to add a catagory

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -6,3 +6,16 @@ from taskmanager.models import Category, Task
 @app.route("/")
 def home():
     return render_template("tasks.html")
+
+@app.route("/catagories")
+def catagories():
+    return render_template("catagories.html")
+
+@app.route("/add_catagories", methods=["GET", "POST"])
+def add_catagories():
+    if request.method == "POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("catagories"))
+    return render_template("add_catagories.html")

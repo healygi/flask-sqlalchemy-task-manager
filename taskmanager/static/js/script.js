8d6cc435d049b40fd14aea03c88d70251d6459db
healygi <healygi@tcd.ie>
2022-06-17 09:11:14 +0000

    Add files and materizle links.

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,6 @@
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // side nav initialization
+    let sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav);
+  });

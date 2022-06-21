691ca01cf564c7bd16fc4b881b621320b859e67a
healygi <healygi@tcd.ie>
2022-06-21 10:38:57 +0000

    Add tasks section

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -3,4 +3,17 @@
     // side nav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(sidenav);
+
+    //date picker
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+      format: "dd mmmm, yyyy",
+      i18n: {done: "Select"}
+
+    });
+
+    // select initialization
+
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
